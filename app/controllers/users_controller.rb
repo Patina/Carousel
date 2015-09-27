bea30414378143bc9873b2966d37f969cda2df69
Jane Alexander <jalexanderny@earthlink.net>
2015-09-27 16:13:13 -0400

    images table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ def set_user
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :avatar)
   end
 end
 
